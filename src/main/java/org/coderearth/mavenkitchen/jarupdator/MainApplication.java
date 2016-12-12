af8adb2e9fe49749ae3fa2f9ec91b590d31c02b8
kunal_patel <kunal_patel@symantec.com>
2016-12-12 10:31:29 +0530

    initial commit

---
+++ src/main/java/org/coderearth/mavenkitchen/jarupdator/MainApplication.java
@@ -0,0 +1,18 @@
+package org.coderearth.mavenkitchen.jarupdator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by kunal_patel on 12/9/16.
+ */
+public class MainApplication {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MainApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Starting Main Application");
+        LOGGER.info("Shutting down Main Application");
+    }
+
+}
